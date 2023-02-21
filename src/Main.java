e58e91139150e2ddeef4fed8461a078648bd4f5c
omm <olgame2027@gmail.com>
2023-02-21 08:54:23 +0300

    added calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!!!  " + args[0]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("x = ");
+        double x = scanner.nextDouble();
+        System.out.print("y = ");
+        double y = scanner.nextDouble();
+        double z = 3.0 * Math.pow(Math.cos(x - Math.PI / 6.0), 2.0) /
+                (0.5 + Math.sin(y) * Math.sin(y) );
+        System.out.println("z = " + z);
     }
 }
